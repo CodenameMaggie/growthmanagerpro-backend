6aca383616a6596bd7cbb5ed9c0c928275fbe518
CodenameMaggie <maggie@maggieforbesstrategies.com>
2025-09-30 10:41:45 -0700

    Update podcast-interviews.js
    key update

--- api/podcast-interviews.js
+++ api/podcast-interviews.js
@@ -1,4 +1,9 @@
-const { supabase } = require('./lib/supabase');
+const { createClient } = require('@supabase/supabase-js');
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
