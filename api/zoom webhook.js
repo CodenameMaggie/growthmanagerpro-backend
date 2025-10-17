069a5019b1b181508cf60980c167dfd26fa954ff
CodenameMaggie <maggie@maggieforbesstrategies.com>
2025-10-17 13:24:21 -0700

    Fix formatting of zoomWebhookSecret variable

--- api/zoom webhook.js
+++ api/zoom webhook.js
@@ -4,7 +4,7 @@ const supabaseUrl = https://tatbblgwhmyzovsyhzyb.supabase.co;
 const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhdGJibGd3aG15em92c3loenliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkxOTQ1NDMsImV4cCI6MjA3NDc3MDU0M30.HUBWBd0Wtdl5rD1G8XSqJe8rYnpMaOXnJiuwHTHyZMo;
 const zoomClientId = GB6HVtuxTPmWqvjS8z5z1w;
 const zoomClientSecret = XM5VWDJtQVRokWAlr3nlg4zKGuOlhE23;
-const zoomWebhookSecret = XM5VWDJtQVRokWAlr3nlg4zKGuOlhE23;
+const zoomWebhookSecret = XM5VWDJtQVRokWAlr3nlg4zKGuOlhE23; 
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
