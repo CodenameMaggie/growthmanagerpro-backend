b220a4d48aaed4f15f228a3a528b2f5f1026ef39
CodenameMaggie <maggie@maggieforbesstrategies.com>
2025-10-28 14:59:48 -0700

    Update login.js

--- api/login.js
+++ api/login.js
@@ -22,10 +22,10 @@ const PERMISSIONS = {
 };
 
 module.exports = async (req, res) => {
- res.setHeader('Access-Control-Allow-Origin', 'https://growthmanagerpro.com');
+ res.setHeader('Access-Control-Allow-Origin', '*');
  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
- res.setHeader('Access-Control-Allow-Credentials', 'true');
+ 
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
