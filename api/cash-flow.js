f2e1b1d9180f415073df4b91766acf1cbd18b0d8
CodenameMaggie <maggie@maggieforbesstrategies.com>
2025-10-28 17:32:12 -0700

    Update cash-flow.js

--- api/cash-flow.js
+++ api/cash-flow.js
@@ -1,8 +1,7 @@
 const { createClient } = require('@supabase/supabase-js');
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_ANON_KEY;
-
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 module.exports = async (req, res) => {
