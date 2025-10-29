eb17deaa64be9ff2babd1cbc9c0cda73d0ed76ae
CodenameMaggie <maggie@maggieforbesstrategies.com>
2025-10-28 17:32:29 -0700

    Update client-dashboard.js

--- api/client-dashboard.js
+++ api/client-dashboard.js
@@ -6,8 +6,8 @@ const { createClient } = require('@supabase/supabase-js');
 
 // Initialize Supabase client
 const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_KEY
+ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 );
 
 module.exports = async (req, res) => {
