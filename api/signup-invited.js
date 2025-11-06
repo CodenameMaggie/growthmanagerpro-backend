ae97f37db1ec76055056da556ea7848db7663056
CodenameMaggie <maggie@maggieforbesstrategies.com>
2025-11-06 14:54:38 -0800

    Update signup-invited.js

--- api/signup-invited.js
+++ api/signup-invited.js
@@ -1,5 +1,4 @@
 const { createClient } = require('@supabase/supabase-js');
-const bcrypt = require('bcryptjs');
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
