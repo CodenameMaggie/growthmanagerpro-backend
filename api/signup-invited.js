342bfbb38e0c09e028c8638838283f6ec831b4e7
CodenameMaggie <maggie@maggieforbesstrategies.com>
2025-11-06 14:25:46 -0800

    Update signup-invited.js

--- api/signup-invited.js
+++ api/signup-invited.js
@@ -5,7 +5,7 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-module.exports = async (req, res) => {
+module.exports = async (req, res) => {   
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
