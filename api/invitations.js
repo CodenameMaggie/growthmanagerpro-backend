1816be6e8e14bf1c7e924dee29e6d171fcfcc24f
CodenameMaggie <maggie@maggieforbesstrategies.com>
2025-11-08 20:25:04 -0800

    Update invitations.js

--- api/invitations.js
+++ api/invitations.js
@@ -6,8 +6,8 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-Tenant-ID');
   
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
